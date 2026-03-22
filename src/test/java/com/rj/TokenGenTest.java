b1f85d0bf105920949f4c1702863294ddfccba0a
jotder <jotder@gmail.com>
2026-03-22 19:38:11 +0530

    Initial Commit

--- src/test/java/com/rj/TokenGenTest.java
+++ src/test/java/com/rj/TokenGenTest.java
@@ -1,9 +1,9 @@
 import fyers.TokenGenerator;
 
 void main() {
-    String appHashID = "40d7592d23f36fa436fc72d60dc6d690d8055feadc8d1f87a28c91fc4e27e6a8";
+    String appHashID = "40d7592d23f36fa436fas2d60dc6d690d805fqwfqtadc8d1f87a28c91fc4e27e6a8";
     String redirectUTL = "https://www.google.com";
-    String clientID = "5UKRW6ONOM-100";
+    String clientID = "5JJKHOKOW-100";
 
     TokenGenerator tokenGenerator = new TokenGenerator();
     String token =tokenGenerator.generateToken(clientID, appHashID, redirectUTL);
